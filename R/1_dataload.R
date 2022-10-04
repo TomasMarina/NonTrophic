3ba2d3b844e8d76d98c20deea92e3fc7b27f25f2
TomasMarina <tomasimarina@gmail.com>
2022-10-04 12:56:24 -0300

    Added competitive interactions suggested by reviewers

--- R/1_dataload.R
+++ R/1_dataload.R
@@ -21,7 +21,8 @@ ipak(packages)
 
 trophic <- read.csv("data/Red_Trofica.csv")
 mutualistic <- read.csv("data/Red_Mutualista.csv")
-competitive <- read.csv("data/Red_Competencia.csv")
+competitive_pred <- read.csv("data/Red_Competencia.csv")
+competitive_res <- read.csv("data/edgelist_competitive_resources.csv")
 commensalistic <- read.csv("data/edgelist_comensalism.csv")
 amensalistic <- read.csv("data/edgelist_amensalism.csv")
 
@@ -44,11 +45,28 @@ edge_mut <- melt(adj_mut) %>%
 g_mut <- graph_from_edgelist(as.matrix(edge_mut[,1:2]), directed = FALSE)
 
 ## Competitive ----
-adj_comp <- as.matrix(competitive[, -1])
-row.names(adj_comp) <- competitive[, 1]
-edge_comp <- melt(adj_comp) %>% 
+# Only btw predators sharing prey
+adj_comp_pred <- as.matrix(competitive_pred[, -1])
+row.names(adj_comp_pred) <- competitive_pred[, 1]
+edge_comp_pred <- melt(adj_comp_pred) %>% 
   filter(value > 0)
-g_comp <- graph_from_edgelist(as.matrix(edge_comp[,1:2]), directed = FALSE)
+g_comp_pred <- graph_from_edgelist(as.matrix(edge_comp_pred[,1:2]), directed = FALSE)
+# Convert and save as edge list
+edgelist_comp_pred <- as_edgelist(g_comp_pred, names = TRUE)
+edgelist_comp_pred_df <- data.frame(edgelist_comp_pred)
+colnames(edgelist_comp_pred_df) <- c("Predator_1","Predator_2")
+#write.csv(edgelist_comp_pred_df, file = "data/edgelist_competitive_predators.csv")
+
+# Bind competitive lists (predators and resources)
+edgelist_comp_pred_ok <- edgelist_comp_pred_df %>% 
+  mutate(TypeCompetition = "Prey") %>% 
+  rename(Competitor_1 = Predator_1, Competitor_2 = Predator_2)
+edgelist_comp_res_ok <- competitive_res %>% 
+  mutate(TypeCompetition = "OtherResources") %>% 
+  dplyr::select(Competitor_1, Competitor_2, TypeCompetition)
+edgelist_comp_final <- bind_rows(edgelist_comp_pred_ok, edgelist_comp_res_ok)
+
+g_comp <- graph_from_edgelist(as.matrix(edgelist_comp_final[,1:2]), directed = FALSE)
 
 ## Commensalistic ----
 g_com <- graph_from_edgelist(as.matrix(commensalistic[,1:2]), directed = FALSE)
